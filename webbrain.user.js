bd4d8ebb552e2155aeda0344a91081b4fe53078f
Mark Robbins <mark.robbins@mrobbinsassoc.com>
2017-03-20 20:04:40 -0700

    script

---
+++ webbrain.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Keyprism3 WebBrain
+// @namespace    http://mrobbinsassoc.com/
+// @version      0.1
+// @description  Allows interaction of TheBrain and Visual Paradigm
+// @author       Mark Robbins
+// @match        https://webbrain.com/brainpage/brain/09335629-E824-3836-6E1A-8A30325D2C09
+// @grant        none
+// @copyright    2017, Mark Robbins
+// @homepageURL  http://mrobbinsassoc.com/projects/keyprism3/vp/publish/
+// @supportURL   mailto:mark.robbins@mrobbinsassoc.com
+// @require      https://code.jquery.com/jquery-3.1.1.min.js
+// ==/UserScript==
+// ==OpenUserJS==
+// @author MarkRobbins
+// ==/OpenUserJS==
+
+
+console.log('Keyprism3 WebBrain UserScript');
